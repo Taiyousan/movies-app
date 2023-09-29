2725727e63826ce17a41acc3bcf0a64c43d8f627
Shams Benhamou <shamsbenhamou@gmail.com>
2023-09-29 09:11:56 +0200

    feat:axios and add style

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,14 +1,30 @@
 <script setup>
-console.log("hello from Home.vue");
+// Imports
+import { onMounted, ref } from "vue";
+import axios from "axios";
+
+let data = ref("");
+
+onMounted(async () => {
+  const response = await axios.get("src/curl/countries.json");
+  data.value = response.data;
+});
 </script>
 
 <template>
-  <header class="header-home"><h1>HELLO</h1></header>
+  <header class="header-home">
+    <h1>ACCUEIL</h1>
+  </header>
 </template>
 
 <style scoped lang="scss">
 .header-home {
   background-color: #252525;
+  width: 100%;
+  height: 100px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
   h1 {
     color: #fff;
   }
