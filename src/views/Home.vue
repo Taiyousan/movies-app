5c6a3e24a8a110b5b528281524ee251cc703bd9e
Shams Benhamou <shamsbenhamou@gmail.com>
2023-09-29 08:59:09 +0200

    feat:install scss

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,11 +3,14 @@ console.log("hello from Home.vue");
 </script>
 
 <template>
-  <h1>HOME</h1>
+  <header class="header-home"><h1>HELLO</h1></header>
 </template>
 
-<style scoped>
-h1 {
-  color: red;
+<style scoped lang="scss">
+.header-home {
+  background-color: #252525;
+  h1 {
+    color: #fff;
+  }
 }
 </style>
