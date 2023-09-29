9b68c71fe6972d5c9388b772900160e9feb8ebd9
Shams Benhamou <shamsbenhamou@gmail.com>
2023-09-29 10:24:35 +0200

    feat:cards

--- src/components/MovieCard.vue
+++ src/components/MovieCard.vue
@@ -1,13 +1,73 @@
-<script setup></script>
+<script setup>
+defineProps({
+  movie: Object,
+});
+</script>
 
 <template>
-  <div class="movie-card"></div>
+  <div class="movie-card">
+    <div class="infos">
+      <h2>{{ movie.title }}</h2>
+      <p><span class="bold">Synopsis : </span> {{ movie.description }}</p>
+      <p><span class="bold">Date de sortie : </span>{{ movie.releaseDate }}</p>
+    </div>
+    <div class="actors">
+      <div class="actor">
+        <p v-for="actor in movie.actors">
+          {{ actor.firstName + " " + actor.lastName }}
+        </p>
+      </div>
+    </div>
+  </div>
 </template>
 
 <style scoped lang="scss">
 .movie-card {
-  width: 100%;
-  height: 100%;
-  background-color: #252525;
+  background-color: #1b1b1b;
+  color: white;
+  padding: 20px;
+  border-radius: 8px;
+  box-shadow: 0 2px 10px rgba(0, 0, 0, 0.1);
+  max-width: 350px;
+  margin: 20px auto;
+
+  .infos {
+    padding: 0 20px 20px;
+    border-bottom: 1px solid rgba(255, 255, 255, 0.1);
+
+    h2 {
+      margin-top: 0;
+      font-size: 1.5em;
+    }
+
+    p {
+      margin: 10px 0;
+      font-size: 0.9em;
+      line-height: 1.4;
+    }
+  }
+
+  .actors {
+    margin-top: 20px;
+    display: flex;
+    flex-wrap: wrap;
+    gap: 10px;
+
+    .actor {
+      flex: 1 1 calc(50% - 10px); // Pour deux acteurs par ligne, adaptez selon le nombre souhaité
+    }
+
+    p {
+      margin: 0;
+      background-color: #2d2d2d;
+      padding: 5px 10px;
+      border-radius: 5px;
+    }
+  }
+
+  .bold {
+    font-weight: bold;
+    color: #f39c12; // couleur d'accent
+  }
 }
 </style>
--- src/views/Movies.vue
+++ src/views/Movies.vue
@@ -1,36 +1,38 @@
-<script setup></script>
+<script setup>
+import { onMounted, ref, toRaw } from "vue";
+import axios from "axios";
+import MovieCard from "../components/MovieCard.vue";
+
+let data = ref("");
+
+onMounted(async () => {
+  const response = await axios.get(
+    "http://localhost/s5/symfony-s5/public/index.php/api/movies?page=1",
+    {
+      headers: {
+        Accept: "application/json",
+      },
+    }
+  );
+  data.value = response.data;
+  console.log(toRaw(data.value));
+});
+</script>
 
 <template>
   <div class="titre"><h1>FILMS</h1></div>
+  <div class="gallery">
+    <MovieCard v-for="movie in data" :key="movie.id" :movie="movie" />
+  </div>
 </template>
 
 <style scoped lang="scss">
-.header-home {
-  background-color: #252525;
-  width: 100%;
-  height: 100px;
+.gallery {
   display: flex;
+  flex-wrap: wrap;
   justify-content: center;
   align-items: center;
-  flex-direction: column;
-  h1 {
-    color: #fff;
-  }
-  nav {
-    width: 100%;
-    height: 50px;
-    display: flex;
-    justify-content: space-around;
-    align-items: center;
-    color: white;
-
-    a {
-      text-decoration: none;
-      color: #252525;
-      padding: 1em;
-      background-color: #fff;
-      border-radius: 5px;
-    }
-  }
+  margin: 2em;
+  gap: 1em;
 }
 </style>
