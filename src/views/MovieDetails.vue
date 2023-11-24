92d6c04c3056c5ed85e045290299e0e4ca88fb28
Shams Benhamou <shamsbenhamou@gmail.com>
2023-11-24 11:37:55 +0100

    feat: fixed details and add loader

--- src/views/MovieDetails.vue
+++ src/views/MovieDetails.vue
@@ -4,34 +4,50 @@ import axios from "axios";
 import { useRoute } from "vue-router";
 import MovieCard from "../components/MovieCard.vue";
 import ActorCard from "../components/ActorCard.vue";
+import PulseLoader from "vue-spinner/src/PulseLoader.vue";
 
 let data = ref("");
 let id = useRoute().params.id;
+let token = localStorage.getItem("token");
+let isLoaded = ref(false);
 
 onMounted(async () => {
-  const response = await axios.get(
-    "http://localhost/s5/symfony-s5/public/index.php/api/movies/" + id,
-    {
-      headers: {
-        Accept: "application/json",
-      },
-    }
-  );
+  const response = await axios.get("http://127.0.0.1:8000/api/movies/" + id, {
+    headers: {
+      Accept: "application/json",
+      Authorization: `Bearer ${token}`,
+      "Content-Type": "application/merge-patch+json",
+    },
+  });
   data.value = response.data;
-  console.log(toRaw(data.value));
+  isLoaded.value = true;
 });
 </script>
 
 <template>
-  <MovieCard :key="data.id" :movie="data" :detailsPage="true" />
-  <div class="gallery">
-    <ActorCard
-      v-for="actor in data.actors"
-      :key="actor.id"
-      :actor="actor"
-      :detailsPage="true"
-    />
+  <div v-if="isLoaded">
+    <MovieCard :key="data.id" :movie="data" :detailsPage="true" />
+    <div class="gallery">
+      <ActorCard
+        v-for="actor in data.actors"
+        :key="actor.id"
+        :actor="actor"
+        :detailsPage="true"
+      />
+    </div>
+  </div>
+
+  <div class="loader-container" v-else>
+    <pulse-loader :loading="loading" color="orange" :size="size"></pulse-loader>
   </div>
 </template>
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+.loader-container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 30vh;
+  width: 100%;
+}
+</style>
