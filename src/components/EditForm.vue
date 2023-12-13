89c3de8c4bfaea5aa79ff13e8f1a925c7c8c5550
Shams Benhamou <shamsbenhamou@gmail.com>
2023-12-13 14:06:21 +0100

    wip: make submit close the modal

--- src/components/EditForm.vue
+++ src/components/EditForm.vue
@@ -1,7 +1,68 @@
 <script setup>
-const props = defineProps(['currentEditingMovie'])
+import { ref } from 'vue'
+import axios from 'axios'
+const token = localStorage.getItem('token')
+const props = defineProps(['currentEditingMovie', 'fetchData', 'handleIsModalEdit'])
+const editedMovie = ref({
+    title: '',
+    description: '',
+    duration: ''
+})
 
-console.log(props.foo)
+// EDIT MOVIE
+async function editMovie() {
+    console.log(props.currentEditingMovie.id);
+    try {
+        // TOKEN
+        // const token = localStorage.getItem("token");
+        // if (!token) {
+        //   router.push("/");
+        //   return;
+        // }
+
+        // HEADERS
+        const headers = {
+            Authorization: `Bearer ${token}`,
+            "Content-Type": "application/merge-patch+json",
+        };
+
+        const updatedMovie = {
+            title: editedMovie.value.title
+                ? editedMovie.value.title
+                : props.currentEditingMovie.title,
+            description: editedMovie.value.description
+                ? editedMovie.value.description
+                : props.currentEditingMovie.description,
+            duration:
+                editedMovie.value.duration !== ""
+                    ? parseInt(editedMovie.value.duration)
+                    : parseInt(props.currentEditingMovie.duration),
+        };
+
+        // REQUEST
+        await axios.patch(
+            `http://127.0.0.1:8000/api/movies/${props.currentEditingMovie.id}`,
+            updatedMovie,
+            { headers }
+        );
+
+        // Recharger les données, fermer le modal, et réinitialiser les valeurs
+        props.handleIsModalEdit(false);
+        props.fetchData();
+        editedMovie.value = {
+            title: "",
+            description: "",
+            duration: "",
+        };
+    } catch (error) {
+        // console.log(error.response.data);
+        // console.log(error.response.status);
+        // console.log(error.response.headers);
+        console.log(error);
+    }
+}
+
+
 </script>
 <template>
     <div class="modal-edit">
@@ -22,8 +83,10 @@ console.log(props.foo)
             <input type="number" id="editDuration" name="editDuration" v-model="editedMovie.duration"
                 :placeholder="currentEditingMovie.duration" />
 
-            <button type="submit">Submit</button>
+            <button type="submit" class="edit-button">Submit</button>
+
         </form>
+        <button @click="props.handleIsModalEdit(false)" class="handle-is-modal-edit">truc</button>
     </div>
 </template>
 <style scoped lang="scss">
@@ -72,9 +135,22 @@ console.log(props.foo)
             height: 300px;
         }
 
-        button {
+        .edit-button {
             padding: 10px;
             background-color: #007bff; // Couleur bleue pour le bouton, ajustez selon vos besoins
+            color: white;
+            border: none;
+            border-radius: 4px;
+            cursor: pointer;
+
+            &:hover {
+                background-color: #0056b3; // Changement de couleur au survol
+            }
+        }
+
+        .handle-is-modal-edit {
+            padding: 10px;
+            background-color: #a7b4c2; // Couleur bleue pour le bouton, ajustez selon vos besoins
             color: white;
             border: none;
             border-radius: 4px;
