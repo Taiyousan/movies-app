ab98d14563d00fb48b8206ede243f3caca756d2c
Shams Benhamou <shamsbenhamou@gmail.com>
2024-03-04 15:51:25 +0100

    fix: fix logo path

--- src/App.vue
+++ src/App.vue
@@ -29,13 +29,12 @@ function logout() {
 
 <template>
   <header class="header-home" v-if="token">
-    <div class="logo-container"><img src="/img/logo.svg" alt=""></div>
     <nav>
-      <RouterLink to="/"><img src="/icons/home.png" alt=""><span>Accueil</span></RouterLink>
-      <RouterLink to="/movies"><img src="/icons/movie.png" alt=""><span>Films</span></RouterLink>
-      <RouterLink to="/actors"><img src="/icons/actor.png" alt=""><span>Acteurs</span></RouterLink>
-      <RouterLink to="/categories"><img src="/icons/category.png" alt=""><span>Catégories</span></RouterLink>
-      <div class="logout" @click="logout()"><img src="/icons/logout.png" alt=""><span>Lougout</span></div>
+      <RouterLink to="/"><img src="icons/home.png" alt=""><span>Accueil</span></RouterLink>
+      <RouterLink to="/movies"><img src="icons/movie.png" alt=""><span>Films</span></RouterLink>
+      <RouterLink to="/actors"><img src="icons/actor.png" alt=""><span>Acteurs</span></RouterLink>
+      <RouterLink to="/categories"><img src="icons/category.png" alt=""><span>Catégories</span></RouterLink>
+      <div class="logout" @click="logout()"><img src="icons/logout.png" alt=""><span>Lougout</span></div>
     </nav>
   </header>
   <div class="main">
